44743b8800bf6e0ce568688f803b715c2b2cc6e9
ejanowski <janowski.emeric@gmail.com>
2020-01-27 16:58:17 +0100

    save / read file

--- app/src/main/java/com/emeric/androidtoolbox/SaveActivity.kt
+++ app/src/main/java/com/emeric/androidtoolbox/SaveActivity.kt
@@ -5,12 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_save.*
+import org.json.JSONObject
+import java.io.File
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class SaveActivity : AppCompatActivity() {
+
+    companion object {
+        val kFirstNameKey = "kFirstNameKey"
+        val kLastNameKey = "kLastNameKey"
+        val kBirthDay = "kBirthDay"
+        val kFilename = "data.json"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save)
+
+        saveButton.setOnClickListener {
+            save()
+        }
+
+        readButton.setOnClickListener {
+            read()
+        }
 
         dateEditText.setOnFocusChangeListener { view, hasFocus ->
             if(hasFocus) {
@@ -27,11 +47,33 @@ class SaveActivity : AppCompatActivity() {
         }
     }
 
+    fun save() {
+        if (firstnameField.text.toString().isNotEmpty() &&
+            lastNameField.text.toString().isNotEmpty() &&
+            dateEditText.text.toString().isNotEmpty()) {
+            val json = JSONObject()
+            json.put(SaveActivity.kFirstNameKey, firstnameField.text.toString())
+            json.put(SaveActivity.kLastNameKey, lastNameField.text.toString())
+            json.put(SaveActivity.kBirthDay, dateEditText.text.toString())
+            Toast.makeText(this, json.toString(), Toast.LENGTH_LONG).show()
+
+            val file = File(cacheDir.absolutePath+"/"+SaveActivity.kFilename)
+            file.writeText(json.toString())
+
+        } else {
+            Toast.makeText(this, R.string.fields_empty, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun read() {
+        val file = File(cacheDir.absolutePath+"/"+SaveActivity.kFilename)
+        val readString = file.readText()
+        val json = JSONObject(readString)
+        Toast.makeText(this,json.getString(SaveActivity.kBirthDay), Toast.LENGTH_LONG).show()
+    }
+
     fun onDateChoose(year: Int, month: Int, day: Int) {
-        dateEditText.setText("${day}/${month+1}/${year}")
-        Toast.makeText(this,
-            "date : ${dateEditText.text.toString()}",
-            Toast.LENGTH_LONG).show()
+        dateEditText.setText(String.format("%02d/%02d/%04d", day, month+1, year))
     }
 
     fun getAge(year: Int, month: Int, day: Int): Int {
